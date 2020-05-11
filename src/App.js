c0f8a7dcb942e04b6f7d2c5b42bafe93986f1316
nicol-hawkins <nicolchawkins@gmail.com>
2020-05-10 17:18:53 -0700

    fixed bug in app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ImageOne from './images/ImageOne.jpg';
+import ImageTwo from './images/ImageTwo.jpg';
+import ImageThree from './images/ImageThree.jpg';
+import ImageFour from './images/ImageFour.jpg';
+import ImageFive from './images/ImageFive.jpg';
+
+let i = 0;
+let images = [];
+let time = 3000;
+
+//Image List
+images[0] = {ImageOne}
+images[1] = {ImageTwo}
+images[2] = {ImageThree}
+images[3] = {ImageFour}
+images[4] = {ImageFive}
+
+//Change Image
+function changeImg() {
+  document.slide.src = images[1];
+
+  if(i < images.length - 1) {
+    i++;
+  } else {
+    i = 0;
+  }
+
+  setTimeout("changeImg()", time)
+}
+
+window.onload = changeImg;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img name="slide" style={{width:"400", height: "auto"}} />
     </div>
   );
 }
